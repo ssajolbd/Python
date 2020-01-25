29dd8c9c75439bf8dc18b8db64f983ad761c26ea
Sajol Sarker <56930742+ssajolbd@users.noreply.github.com>
2020-01-25 12:03:21 -0800

    Add files via upload

---
+++ 10quize.py
@@ -0,0 +1,98 @@
+# Quize list 10
+
+# database
+game0 = "yes"
+
+game = input("Are You Ready To Play : ")
+if game == game0:
+    print("welcome")
+
+    # database
+    quize1 = "Jackson Atlanta"  # world largest airport
+    quize2 = " Tokyo "  # world largest city
+    quize3 = " Sundar Pichai "  # Google ceo
+    quize4 = " Justin Trudeau "  # canadan prime minister
+    quize5 = " China "  # which country has the largest population 2019
+    quize6 = " Mark Zuckerberg "  # Facebook ceo
+    quize7 = " CALGARY "  # clean city in the world
+    quize8 = " Rajasthan "  # one of the largest state of india
+    quize9 = " Mumbai "  # highest population city in india
+    quize10 = "Abdus Suttar Khan / suttar khan / abdus sutter "
+    # bangladeshi scientist job at nasa ..?
+
+    # 1
+
+    quize1 = input(
+        "What is the name of the largest airport in the world ...?  :")
+        elif quize1 == quize1:
+        print("10 marks & Go to 2nd")
+
+        # 2
+
+        quize2 = input(
+            "What is the name of the largest city in the world ...?  : ")
+        elif quize2 == quize2:
+            print("10 marks & Go to 3rd")
+
+        # 3
+
+        quize3 = input(
+            "Who is the CEO and founder of Google ...?  : ")
+        elif quize3 == quize3:
+            print("10 marks & Go to 4th")
+
+        # 4
+
+        quize4 = input(
+            "What is the name of the Prime Minister of Canada?  : ")
+        elif quize4 == quize4:
+            print("10 marks & Go to 5th")
+
+        # 5
+
+        quize5 = input(
+            "which country has the largest population in 2019 ...?  : ")
+        elif quize5 == quize5:
+            print("10 marks & Go to 6th")
+
+        # 6
+
+        quize6 = input(
+            "Who is the CEO and founder of Facebook ...?  : ")
+        elif quize6 == quize6:
+            print("10 marks & Go to 7th")
+
+        # 7
+
+        quize7 = input(
+            "which city has the cleanest city in world ...?   : ")
+        elif quize7 == quize7:
+            print("10 marks & Go to 8th")
+
+        # 8
+
+        quize8 = input(
+            "which city has the largest city in india...?  : ")
+        elif quize8 == quize8:
+            print("10 marks & Go to 9th")
+
+        # 9
+
+        quize9 = input(
+            "highest population city in india  : ")
+        elif quize9 == quize9:
+            print("10 marks & Go to 9th")
+
+        # 10
+
+        quize10 = input(
+            "which bangladeshi scientist has job at nasa ..?  : ")
+        elif quize10 == quize10:
+            print(
+                "Congratulations You Have Been Won This Game & Your Totel Point Is -100  ")
+
+    else:
+        print("sorry try again")
+
+else:
+    print("See You Soon")
